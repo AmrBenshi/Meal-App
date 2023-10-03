effc7c2dc67b562775367d88edb90e55d243b472
AmrBenshi <amrsaeedbenshi@gmail.com>
2023-10-03 18:43:54 +0300

    UP

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -17,7 +17,6 @@ const cartReducer = (state, action) => {
     );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
-
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
@@ -54,7 +53,7 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   }
-
+  if (action.type === 'CLEAR') return defaultCartState;
   return defaultCartState;
 };
 
@@ -71,12 +70,15 @@ const CartProvider = (props) => {
   const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
-
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' })
+  }
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
